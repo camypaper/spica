e58225cba807ffd61ba96ab8614509d17babb563
camypaper <camypaper@vivaldi.net>
2019-05-06 12:27:31 +0900

    add generator

---
+++ core/generator.go
@@ -0,0 +1,34 @@
+package core
+
+import (
+	"fmt"
+
+	"github.com/Camypaper/libra"
+
+	"github.com/Sirupsen/logrus"
+)
+
+/*
+Generator setting.
+*/
+type Generator struct {
+	Name string `toml:"name"`
+	Cnt  int    `toml:"cnt"`
+}
+
+/*
+ToJob converts to GenJob
+*/
+func (generator Generator) ToJob(config Config) (libra.Job, error) {
+	lang, err := config.Find(generator.Name)
+	if err != nil {
+		logrus.WithError(err).Errorf("language for %v does not found.", generator.Name)
+		return nil, err
+	}
+	src := libra.Src{Name: generator.Name, Compile: lang.Compile, Exec: lang.Exec}
+	return libra.GenJob(src, generator.Cnt), nil
+}
+
+func (generator Generator) String() string {
+	return fmt.Sprintf("[Name:%v, Cnt:%v]", generator.Name, generator.Cnt)
+}
